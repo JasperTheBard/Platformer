0d5ad5dbdcf6e1db8b31fe8e48cb4011eb9c3260
Jasper Kofron <43767829+JasperTheBard@users.noreply.github.com>
2018-10-02 14:58:21 +1000

    Maps and camera
    added maps and a camera veiwport

---
+++ Platformer/Game1/Content/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Tileset" tilewidth="64" tileheight="64" tilecount="72" columns="8">
+ <image source="../../../../../../source/repos/Platformer/Game1/Content/Tileset.png" width="512" height="576"/>
+</tileset>
--- Platformer/Game1/Game1.cs
+++ Platformer/Game1/Game1.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Graphics;
+using MonoGame.Extended.ViewportAdapters;
 
 namespace Game1
 {
@@ -12,6 +16,13 @@ namespace Game1
 
 
         Player player = new Player(); //Creates an instance of our player in this class
+
+        Camera2D camera = null; //Creates an instance of our players
+        TiledMap map = null; //Creates an instance of a Tilted map
+        TiledMapRenderer mapRendered = null; // Creates an Instance of what makes a tilted map 
+
+
+
 
         public Game1()
         {
@@ -32,6 +43,11 @@ namespace Game1
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             player.Load(Content); //call the 'Load' function in the player class
+            BoxingViewportAdapter viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, graphics.GraphicsDevice.Viewport.Height, graphics.GraphicsDevice.Viewport.Width);
+
+            map = Content.Load<TiledMap>("Level1");
+            mapRendered = new TiledMapRenderer(GraphicsDevice);
+
         }
 
         protected override void UnloadContent()
@@ -52,8 +68,12 @@ namespace Game1
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.LightSlateGray);
+
+            var viewMatrix = camera.GetViewMatrix();
+            var projectionMatrix = Matrix.CreateOrthographicOffCenter(0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, 0, 0.0f, -0.0f);
             spriteBatch.Begin();
 
+            mapRendered.Draw(map, ref viewMatrix, ref projectionMatrix);
             player.Draw(spriteBatch);
 
             spriteBatch.End();
