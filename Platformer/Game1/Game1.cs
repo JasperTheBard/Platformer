e842e94e3b97702895a04dfb8b0e1949ca40b924
Jasper Kofron <43767829+JasperTheBard@users.noreply.github.com>
2018-10-02 11:21:26 +1000

    new platformer game
    added player and sprite classes

---
+++ Platformer/Game1/Game1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+
+        Player player = new Player(); //Creates an instance of our player in this class
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+
+
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            player.Load(Content); //call the 'Load' function in the player class
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            player.Update(deltaTime); //call the 'Update' function from the player class
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.LightSlateGray);
+            spriteBatch.Begin();
+
+            player.Draw(spriteBatch);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Platformer/Game1/sprite.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    class sprite
+    {
+        new Vector2 position = Vector2.Zero;
+        new Vector2 offSet = Vector2.Zero;
+
+        Texture2D texture;
+
+        public sprite()
+        {
+
+        }
+
+        public void Load(ContentManager content, string assist)
+        {
+            texture = content.Load<Texture2D>(assist);
+
+
+        }
+        public void Update(float deltaTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position = offSet, Color.White);
+        }
+    }
+}
